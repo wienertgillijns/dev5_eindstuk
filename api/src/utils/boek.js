572bd9008c07ad14a4dc3272ee06b3c7736f3137
Wienert Gillijns <wienert.gillijns@student.ehb.be>
2021-01-17 20:22:09 +0100

    crud

--- api/src/utils/boek.js
+++ api/src/utils/boek.js
@@ -1,5 +1,6 @@
 
 const {v1: uuidv1 } = require('uuid');
+const app = require('../server');
 
 const Helpers = {
   generateUUID: () => {
@@ -7,5 +8,34 @@ const Helpers = {
      return uuid;
   }
 }
+
+async function boekenData(){
+  let boekenlistList = []
+  await pg.schema.hasTable('books').then(async (exists) => {
+    await pg.schema.createTable('books', (table) => {
+      table.string(naamBoek)
+      table.string(genreBoek)
+      table.string(auteurBoek)
+
+    }
+    )}
+  )}
+  app.get('/api/:uuid', async (req, res) => {
+    
+  })
+  app.delete('/api/:uuid', async (req, res) => {
+
+    try {
+      await pg('api').where({uuid: uuid}).del().then(data => {
+          res.status(300).send()
+          console.log(`Book deleted`)
+
+      })
+      
+  }catch(e){
+      res.status(400).send("Problem with deleting book")
+  }
+  })
+
 
 module.exports = Helpers
